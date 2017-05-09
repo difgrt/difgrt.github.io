64a45264ed5f35c972e8b0f428f9a9f69dd112b4
2011004192 <2011004192@hanyang.ac.kr>
2017-05-09 22:13:05 +0900

    Commit

---
+++ script.js
@@ -0,0 +1,50 @@
+var jtbc, sbs, kbs;
+var i = 0;
+var activePlayer = null;
+var activeWrp = null;
+var listener = new window.keypress.Listener();
+
+function createPlayer(bcID, vID) {
+    var p = new YT.Player(bcID, {
+        height: '100%',
+        width: '100%',
+        videoId: vID,
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+    return p;
+}
+
+function onPlayerReady(e) {
+    e.target.mute();
+    e.target.playVideo();
+    i++;
+    if ( i == 3 ) {
+        jtbc.unMute();
+        $('#jtbc_wrp').addClass('active');
+        activePlayer = jtbc;
+        activeWrp = $('#jtbc_wrp');
+        listener.simple_combo('1', activePlayerFn(jtbc, $('#jtbc_wrp')));
+        listener.simple_combo('2', activePlayerFn(sbs, $('#sbs_wrp')));
+        listener.simple_combo('3', activePlayerFn(kbs, $('#kbs_wrp')));
+    }
+}
+
+function activePlayerFn(target, targetDiv) {
+    return function() {
+        activePlayer.mute();
+        activeWrp.removeClass('active')
+        target.unMute();
+        targetDiv.addClass('active');
+        activePlayer = target;
+        activeWrp = targetDiv;
+    }
+}
+
+function onYouTubeIframeAPIReady() {
+    jtbc = createPlayer('jtbc', 'GpUD0yWA6ZQ');
+    sbs = createPlayer('sbs', 'lo7tgFI8Q9I');
+    kbs = createPlayer('kbs', 'mHR_bM2BWqQ');
+}
+
